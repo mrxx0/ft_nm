f1e5dd0b4032224627db4b88e397735f484a955d
mrxx0 <chcoutur@student.42.fr>
2021-04-13 12:31:13 +0200

    Add old libft

---
+++ srcs/libft/includes/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chcoutur <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/04 18:05:26 by chcoutur          #+#    #+#             */
+/*   Updated: 2019/07/03 18:49:26 by charles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft.h"
+
+# define BUFF_SIZE 100
+
+int		get_next_line(const int fd, char **line);
+
+#endif
