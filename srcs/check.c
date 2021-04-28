7662f49e1d0c9a55132cb261072ccb04f9a8babc
mrxx0 <chcoutur@student.42.fr>
2021-04-28 17:08:05 +0200

    Add new check file for all checking related functions

---
+++ srcs/check.c
@@ -0,0 +1,22 @@
+#include "../includes/ft_nm.h"
+
+int check_file_is_elf(char *mmap_return, char *file_offset, char *file_name)
+{
+	(void) file_name;
+	if (mmap_return > file_offset)
+		return (ft_perror("File is corrupted\n", 0));
+	if ((unsigned char)mmap_return[EI_MAG0] == 0x7f &&
+		(unsigned char)mmap_return[EI_MAG1] == 'E' &&
+		(unsigned char)mmap_return[EI_MAG2] == 'L' &&
+		(unsigned char)mmap_return[EI_MAG3] == 'F')
+	{
+		ft_printf("File is ELF format !\n");
+		return (EXIT_SUCCESS);
+	}
+	else
+		return (ft_perror("File is not ELF format\n", 0));
+
+
+(void)file_name;
+	return (EXIT_SUCCESS);
+}
