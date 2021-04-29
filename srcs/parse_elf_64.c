bcb3edbfb347ee01e070d000d418a42ded7317b8
mrxx0 <chcoutur@student.42.fr>
2021-04-29 11:07:18 +0200

    Indent code + add reverse_for_64 function

--- srcs/parse_elf_64.c
+++ srcs/parse_elf_64.c
@@ -6,11 +6,16 @@ int	parse_elf_64(char *mmap_return, char *file_offset)
 	_Bool		system_endian = 0;
 	_Bool		reverse_endian = 0;
 	Elf64_Ehdr	*elf_header;
+	Elf64_Shdr	*elf_shdr;
 	
 	elf_header = (Elf64_Ehdr *)mmap_return;
 	file_endian = get_endian_file(elf_header);
 	system_endian = get_endian_system();
 	reverse_endian = need_to_reverse(file_endian, system_endian);
+	if (reverse_endian == TRUE)
+		elf_shdr = (Elf64_Shdr *)(mmap_return + reverse_for_64(elf_header->e_shoff));
+	else
+		elf_shdr = (Elf64_Shdr *)(mmap_return + elf_header->e_shoff);
 	(void)mmap_return;
 	(void)file_offset;
 	return (EXIT_SUCCESS);
