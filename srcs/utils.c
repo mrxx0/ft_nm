9b0b37278ce1b590eb24226ef73ba6da4ad3dddf
mrxx0 <chcoutur@student.42.fr>
2021-04-29 10:24:20 +0200

    Move endian check function to a separate files

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,32 +1,5 @@
 #include "../includes/ft_nm.h"
 
-_Bool	need_to_reverse(_Bool file_endian, _Bool system_endian)
-{
-	if (file_endian != system_endian)
-		return (TRUE);
-	else
-		return (FALSE);
-}
-
-_Bool	get_endian_file(Elf64_Ehdr *elf_header)
-{
-	if (elf_header->e_ident[EI_DATA] == ELFDATA2LSB)
-		return (LITTLE);
-	else
-		return (BIG);
-}
-
-_Bool	get_endian_system(void)
-{
-	uint8_t x = 1;
-	char *y = (char*)&x;
-
-	if (*y == LITTLE)
-		return (LITTLE);
-	else
-		return (BIG);
-}
-
 int	open_file(char *file_name, int *fd, char **mmap_return,  struct stat *stat)
 {
 	*fd = open(file_name, O_RDONLY);
