7ce5ed0e68db0259cbb7b6b042f9c02e3bc18bf5
mrxx0 <chcoutur@student.42.fr>
2021-05-06 18:43:29 +0200

    Fix error return

--- srcs/main.c
+++ srcs/main.c
@@ -11,9 +11,9 @@ int main(int argc, char **argv)
 	while (i < argc)
 	{	
 		//output format
-		if (open_file(argv[i], &fd, &mmap_return, &stat) == FAILURE)
+		if (open_file(argv[i], &fd, &mmap_return, &stat) == EXIT_FAILURE)
 			return (EXIT_FAILURE);
-		if ((check_file_is_elf(mmap_return, mmap_return + stat.st_size, argv[i]) == FAILURE))
+		if ((check_file_is_elf(mmap_return, mmap_return + stat.st_size, argv[i]) == EXIT_FAILURE))
 			return (EXIT_FAILURE);
 		i++;
 	}
