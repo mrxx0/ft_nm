6264e6d592b8e791508769377a1c2fffd1492eb9
mrxx0 <chcoutur@student.42.fr>
2021-05-07 14:27:12 +0200

    Print test on ar file

--- srcs/parse_ar.c
+++ srcs/parse_ar.c
@@ -3,6 +3,7 @@
 int	parse_ar(char *mmap_return, char *file_offset)
 {
     struct ar_hdr *ar;
+    ft_printf("mmap_return + SARMAG = [%s]\n", mmap_return + SARMAG);
 
     ar = (struct ar_hdr *)(mmap_return + SARMAG);
     ft_printf("ar_name = [%s]\n", ar->ar_name);
@@ -13,7 +14,11 @@ int	parse_ar(char *mmap_return, char *file_offset)
     ft_printf("ar_size = [%s]\n", ar->ar_size);
     ft_printf("ar_fmag = [%s]\n", ar->ar_fmag);
 
-   // ft_printf("%s\n", mmap_return);
+    mmap_return = mmap_return + SARMAG;
+    ft_printf("mmap_return = [%s]\n", mmap_return);
+    
+    mmap_return = mmap_return + ft_atoi(ar->ar_size);
+    ft_printf("mmap_return = [%s]\n", mmap_return);
     if (mmap_return > file_offset)
 		return (ft_perror("File is corrupted\n", 0));
     
