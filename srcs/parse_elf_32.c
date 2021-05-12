c8a4a2921584aaff2ba7aa95900e4de19fdc07a0
mrxx0 <chcoutur@student.42.fr>
2021-05-12 17:13:48 +0200

    Print for 32 is OK

--- srcs/parse_elf_32.c
+++ srcs/parse_elf_32.c
@@ -1,21 +1,5 @@
 #include "../includes/ft_nm.h"
 
-uint32_t			reverse32(uint32_t x, short reverse) {
-	int				i = 0;
-	uint32_t		y = 0;
-	unsigned char	*ptr_x, *ptr_y;
-	size_t size = sizeof(x);
-
-	if (!reverse)
-		return (x);
-	ptr_x = (unsigned char *)&x;
-	ptr_y = (unsigned char *)&y;
-	while (--size)
-		ptr_y[i++] = ptr_x[size];
-	ptr_y[i++] = ptr_x[size];
-	return (y);
-}
-
 int	parse_elf_32(char *mmap_return, char *file_offset)
 {
 	(void)file_offset;
@@ -25,37 +9,25 @@ int	parse_elf_32(char *mmap_return, char *file_offset)
 	_Bool			system_endian = 0;
 	_Bool			reverse = 0;
 	char			*elf_strtable;
-	// t_elf_section_part	*elf_sections = NULL;
+	t_elf_section_part	*elf_sections = NULL;
 
 	elf_header = (Elf32_Ehdr *)mmap_return;
+		
 	file_endian = get_endian_file_32(elf_header);
 	if (file_endian == FAILURE)
 		return(ft_perror("File format not recognized\n", 0));
 	system_endian = get_endian_system();
 	reverse = need_to_reverse(file_endian, system_endian);
-	if (reverse)
-		ft_printf("NEED TO REVERSE\n");
-	// elf_shdr = (Elf32_Shdr *)(mmap_return + (uint32_t)reverse_for_32(elf_header->e_shoff, reverse));
-	elf_shdr = (Elf32_Shdr *)(mmap_return + reverse_for_32(elf_header->e_shoff, reverse));
-		ft_printf("before\n");
-	// elf_strtable = mmap_return + reverse_for_32(elf_shdr[reverse_for_32(elf_header->e_shstrndx, reverse)].sh_offset, reverse);
-	// printf("shstrndx = %u\n", elf_header->e_shstrndx);
-	// printf("shstrndx = %u\n", reverse_for_32(elf_header->e_shstrndx, reverse));
-	printf("sh_offset = %u\n", elf_shdr[elf_header->e_shstrndx].sh_offset);
-	// printf("sh_offset = %u\n", reverse_for_32(elf_shdr[reverse_for_32(elf_header->e_shstrndx, reverse)].sh_offset, reverse));
-	printf("%u\n", reverse_for_32(elf_shdr[elf_header->e_shstrndx].sh_offset, reverse));
-	printf("%u\n", reverse_for_32(elf_header->e_shstrndx, reverse));
-	printf("%u\n", elf_shdr[2572100].sh_offset);
-
-	(void)elf_strtable;
-	// elf_strtable = mmap_return + elf_shdr[reverse32(elf_header->e_shstrndx, reverse)].sh_offset;
-		ft_printf("ok\n");
-
-	// elf_strtable = mmap_return + (elf_shdr[elf_header->e_shstrndx].sh_offset);
-		// ft_printf("ok\n");
-
-	// elf_sections = stock_elf32_sections((uint16_t)reverse_for_32(elf_header->e_shnum, reverse), elf_shdr, elf_strtable, reverse);
-		// ft_printf("okok\n");
-
+	elf_shdr = (Elf32_Shdr *)(mmap_return + (uint32_t)reverse_for_32(elf_header->e_shoff, sizeof(elf_header->e_shoff), reverse));
+	elf_strtable = mmap_return + reverse_for_32(elf_shdr[(uint16_t)reverse_for_32(elf_header->e_shstrndx,sizeof(elf_header->e_shstrndx), reverse)].sh_offset, sizeof(elf_shdr->sh_offset), reverse);
+	elf_sections = stock_elf32_sections((uint16_t)reverse_for_32(elf_header->e_shnum, sizeof(elf_header->e_shnum), reverse), elf_shdr, elf_strtable, reverse);
+	if (elf_sections == MALLOC_FAILED)
+		return (ft_perror("Malloc failed to allocate memory\n.", 0));
+	if (parse_elf32_symbols(elf_header, elf_shdr, elf_sections, reverse) == FALSE)
+	{
+		if (elf_sections)
+			free(elf_sections);
+		return (ft_perror("No symbol\n", 0));
+	}
 	return (EXIT_SUCCESS);
 }
