d95aff351cef8c6730700d816548501a509057cf
mrxx0 <chcoutur@student.42.fr>
2021-05-13 11:11:46 +0200

    Update prototype for reverse_for_64 with new parameter

--- srcs/parse_elf_64.c
+++ srcs/parse_elf_64.c
@@ -19,9 +19,9 @@ int	parse_elf_64(char *mmap_return, char *file_offset)
 		return(ft_perror("File format not recognized\n", 0));
 	system_endian = get_endian_system();
 	reverse = need_to_reverse(file_endian, system_endian);
-	elf_shdr = (Elf64_Shdr *)(mmap_return + (uint32_t)reverse_for_64(elf_header->e_shoff, reverse));
-	elf_strtable = mmap_return + reverse_for_64(elf_shdr[(uint16_t)reverse_for_64(elf_header->e_shstrndx, reverse)].sh_offset, reverse);
-	elf_sections = stock_elf64_sections((uint16_t)reverse_for_64(elf_header->e_shnum, reverse), elf_shdr, elf_strtable, reverse);
+	elf_shdr = (Elf64_Shdr *)(mmap_return + (uint32_t)reverse_for_64(elf_header->e_shoff, sizeof(elf_header->e_shoff), reverse));
+	elf_strtable = mmap_return + reverse_for_64(elf_shdr[(uint16_t)reverse_for_64(elf_header->e_shstrndx, sizeof(elf_header->e_shstrndx), reverse)].sh_offset, sizeof(elf_shdr->sh_offset), reverse);
+	elf_sections = stock_elf64_sections((uint16_t)reverse_for_64(elf_header->e_shnum, sizeof(elf_header->e_shnum), reverse), elf_shdr, elf_strtable, reverse);
 	if (elf_sections == MALLOC_FAILED)
 		return (ft_perror("Malloc failed to allocate memory\n.", 0));
 	if (parse_elf64_symbols(elf_header, elf_shdr, elf_sections, reverse) == FALSE)
