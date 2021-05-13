882f894ba63b75fb0e6d6762b332647c67618758
mrxx0 <chcoutur@student.42.fr>
2021-05-13 09:42:17 +0200

    Update diff script

--- diff.sh
+++ diff.sh
@@ -3,11 +3,16 @@
 ./ft_nm $1 > ft_nm.txt
 nm -p $1 > nm.txt
 
-echo $1
 
-echo "Diff START"
-diff -uN ft_nm.txt nm.txt
-echo "Diff END"
-echo -e "\n"
+DIFF=$(diff -uN ft_nm.txt nm.txt)
+
+if [ "$DIFF" != "" ]
+then
+	echo $1
+	echo "Diff START"
+	echo $DIFF
+	echo "Diff END"
+	echo -e "\n"
+fi
 rm -r ft_nm.txt nm.txt
 
